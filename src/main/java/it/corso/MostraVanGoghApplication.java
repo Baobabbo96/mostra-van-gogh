b252fdb9bb0a90de2d3ee0ed3ecf7aa3740b3a70
Baobabbo96 <s.mluongo96@gmail.com>
2023-05-22 15:13:08 +0200

    Creazione progetto

---
+++ src/main/java/it/corso/MostraVanGoghApplication.java
@@ -0,0 +1,13 @@
+package it.corso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MostraVanGoghApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MostraVanGoghApplication.class, args);
+	}
+
+}
