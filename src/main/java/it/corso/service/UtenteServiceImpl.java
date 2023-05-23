1916b51ec5ee33391780f8902aef07aed8a2b75e
Baobabbo96 <s.mluongo96@gmail.com>
2023-05-23 10:06:04 +0200

    Service impl

---
+++ src/main/java/it/corso/service/UtenteServiceImpl.java
@@ -0,0 +1,33 @@
+package it.corso.service;
+
+import java.util.List;
+
+import it.corso.model.Utente;
+
+public class UtenteServiceImpl implements UtenteService {
+
+	@Override
+	public void registraUtente(Utente utente, String... dati) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Utente getUtenteById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Utente> getUtenti() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void cancellaUtente(Utente utente) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
