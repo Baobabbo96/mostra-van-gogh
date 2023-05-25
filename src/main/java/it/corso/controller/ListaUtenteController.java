95c6014966772ccb1ee61e6599c96361acf6ca33
JacopoLobina1 <lobina.jacopo@gmail.com>
2023-05-25 17:55:38 +0200

    Update ListaUtenteController.java

--- src/main/java/it/corso/controller/ListaUtenteController.java
+++ src/main/java/it/corso/controller/ListaUtenteController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import it.corso.service.UtenteService;
 import jakarta.servlet.http.HttpSession;
-import jakarta.websocket.Session;
 
 @Controller
 @RequestMapping("/list")
