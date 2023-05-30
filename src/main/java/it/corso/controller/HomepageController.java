27d6f5d1f60a8cc273b89f2326d844eedc3103fa
Baobabbo96 <s.mluongo96@gmail.com>
2023-05-30 12:00:37 +0200

    botton login account

--- src/main/java/it/corso/controller/HomepageController.java
+++ src/main/java/it/corso/controller/HomepageController.java
@@ -1,15 +1,25 @@
 package it.corso.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpSession;
+import jakarta.websocket.Session;
 
 @Controller
 @RequestMapping("/home")
 public class HomepageController {
 
 	@GetMapping
-	public String getPage() {
+	public String getPage(
+			HttpSession session,
+			Model model) {
+		boolean logged = false;
+		if (session.getAttribute("utente") != null)
+			logged = true;
+		model.addAttribute("logged", logged);
 		return "home";
 	}
 }
