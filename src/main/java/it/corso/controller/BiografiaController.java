612c5ada3ff48e8aed181ad30b01dae33e125db2
Baobabbo96 <s.mluongo96@gmail.com>
2023-05-31 14:26:16 +0200

    bottone login/account cambiato in tutte le pagine

--- src/main/java/it/corso/controller/BiografiaController.java
+++ src/main/java/it/corso/controller/BiografiaController.java
@@ -1,16 +1,25 @@
 package it.corso.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/biografia")
 public class BiografiaController {
 	
 	@GetMapping
-	public String getPage() 
+	public String getPage(
+			Model model,
+			HttpSession session) 
 	{
+		boolean logged = false;
+		if (session.getAttribute("utente") != null)
+			logged = true;
+		model.addAttribute("logged", logged);
 		return"biografia";
 	}
 
